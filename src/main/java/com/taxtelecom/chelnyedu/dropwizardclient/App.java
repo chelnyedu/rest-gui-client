e96021563ac374d26afb73bf239b56836c2d2412
sagel@bk.ru <git_rabota2017>
2017-07-21 15:40:08 +0300

    refactor client

--- src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/App.java
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/App.java
@@ -1,67 +1,51 @@
 package com.taxtelecom.chelnyedu.dropwizardclient;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
 import com.taxtelecom.chelnyedu.dropwizardclient.client.ContactApi;
+import com.taxtelecom.chelnyedu.dropwizardclient.client.RetrofitClient;
 import com.taxtelecom.chelnyedu.dropwizardclient.resources.Contact;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by user on 18.07.17.
  */
 public class App {
-    private static ContactApi service;
+    public static ContactApi service;
     public static void main(String[] args) throws IOException {
         System.out.println("куку");
+        RetrofitClient rc = new RetrofitClient();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://localhost:8080")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         service = retrofit.create(ContactApi.class);
-//getAll--------------------------------------------------------
-        Call<List<Contact>> call = service.getAllContact();
 
-        List<Contact> contactList = call.execute().body();
-        for (Contact c: contactList){
+        //getContactByID
+        Contact g = rc.gContact(4);
+        System.out.println(g.getId() + " " + g.getFirstName() + " " + g.getLastName() + " " + g.getPhone() + " " + g.getMail() + " " + g.getComment());
+
+
+        //getList
+        List<Contact> lists = rc.contactList();
+        for (Contact c : lists) {
             System.out.println(c.getId()+" "+ c.getFirstName());
         }
 
-        //getContact------------------------------------------------------
-        Call<Contact> c1=service.getContact(4);
-        Contact c=c1.execute().body();
-        System.out.println(c.getId() +" "+c.getFirstName()+" "+c.getLastName()+" "+ c.getPhone()+" " + c.getMail()+" " +c.getComment());
-//deleteContact------------------------------------------------------
-        try {
-            Call<Contact> del = service.deleteContact(17);
-            System.out.println(service.deleteContact(17).execute().raw());
-            //System.out.println("Contact was deleted");
-        }catch (Throwable t){
-            System.out.println("Something wrong! Contact was not deleted!!!!!!!!!!!!!!!!!!!!!");
-        }
-        //createContact---------------------------------------------------------------
+
+        //deleteContact
+        System.out.println(rc.delContact(19));
+
+        //createContact
         Contact cr=new Contact(1,"Olya", "Kuzmina", "9875123", "rere", "cool" );
-        Call<Contact> create = service.createContact(cr);
+        System.out.println(rc.creaContact(cr));
 
-
-        //updateContact--------------------------------------------------------------
-
+        //updateContact
         Contact contactforupdate = new Contact(4, "Jane", "Doe", "+987654321", "rere", "qwerty");
-        Call<Contact> update=service.updateContact(4, contactforupdate);
-        System.out.println(update.execute().raw());
-
+        System.out.println(rc.upContact(contactforupdate));
     }
-
     public static ContactApi getApi(){
         return service;
     }
--- src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/client/RetrofitClient.java
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/client/RetrofitClient.java
@@ -6,11 +6,49 @@ import retrofit2.Call;
 import java.io.IOException;
 import java.util.List;
 
+import static com.taxtelecom.chelnyedu.dropwizardclient.App.service;
+
 /**
  * Created by user on 21.07.17.
  */
 public class RetrofitClient {
+    //private static ContactApi service = App.getApi();
+    String ok = "OK";
+    String wrong = "WRONG";
 
+    public Contact gContact(int id) throws IOException {
+        Call<Contact> c1 = service.getContact(id);
+        Contact c = c1.execute().body();
+        //System.out.println(c.getId() +" "+c.getFirstName()+" "+c.getLastName()+" "+ c.getPhone()+" " + c.getMail()+" " +c.getComment());
+        return c;
+    }
 
+    public List<Contact> contactList() throws IOException {
+        Call<List<Contact>> call = service.getAllContact();
 
+        List<Contact> contacts = call.execute().body();
+       /* for (Contact c: contacts){
+            System.out.println(c.getId()+" "+ c.getFirstName());
+        }*/
+        return contacts;
+    }
+
+    public String delContact(int id) throws IOException {
+        Call<Contact> del = service.deleteContact(id);
+        if (del.execute().isSuccessful()) {
+            return ok;
+        } else return wrong;
+    }
+
+    public String creaContact(Contact contact) throws IOException {
+        Call<Contact> create = service.createContact(contact);
+        return ok;
+    }
+
+    public String upContact(Contact contact) throws IOException {
+        Call<Contact> update = service.updateContact(contact.getId(), contact);
+        if (update.execute().isSuccessful()) {
+            return ok;
+        } else return wrong;
+    }
 }
