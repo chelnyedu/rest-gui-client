4a375c624d2616d5345654132b97ffb799c3c46f
sagel@bk.ru <git_rabota2017>
2017-07-24 11:48:39 +0300

    Contact.class

---
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/resources/Contact.java
@@ -0,0 +1,77 @@
+package com.taxtelecom.chelnyedu.dropwizardclient.resources;
+
+/**
+ * Created by user on 20.07.17.
+ */
+public class Contact {
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final String phone;
+    private final String mail;
+    private final String comment;
+
+    public Contact(){
+        this.id = 0;
+        this.firstName = null;
+        this.lastName = null;
+        this.phone = null;
+        this.comment = null;
+        this.mail = null;
+    }
+
+    public Contact(int id, String firstName, String lastName, String phone, String mail, String comment){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.mail = mail;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getMail(){ return mail; }
+
+    public String getComment(){ return comment; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        if (id != contact.id) return false;
+        if (firstName != null ? !firstName.equals(contact.firstName) : contact.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(contact.lastName) : contact.lastName != null) return false;
+        if (phone != null ? !phone.equals(contact.phone) : contact.phone != null) return false;
+        if (mail != null ? !mail.equals(contact.mail) : contact.mail != null) return false;
+        return comment != null ? comment.equals(contact.comment) : contact.comment == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (mail != null ? mail.hashCode() : 0);
+        result = 31 * result + (comment != null ? comment.hashCode() : 0);
+        return result;
+    }
+}
