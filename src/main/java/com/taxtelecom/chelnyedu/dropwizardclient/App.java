8fa7cc631a18ef2b29a1f77829dc3ec35716dc60
sagel@bk.ru <git_rabota2017>
2017-07-24 11:46:19 +0300

    some sonar

--- src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/App.java
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/App.java
@@ -1,21 +1,9 @@
 package com.taxtelecom.chelnyedu.dropwizardclient;
-
-import com.taxtelecom.chelnyedu.dropwizardclient.client.ContactApi;
-import com.taxtelecom.chelnyedu.dropwizardclient.client.RetrofitClient;
-import com.taxtelecom.chelnyedu.dropwizardclient.resources.Contact;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import java.io.IOException;
-import java.util.List;
-
 /**
  * Created by user on 18.07.17.
  */
 public class App {
-    public static ContactApi service;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("куку");
     }
 }
