a4c3dea41ade4a0f7261cd886040c1a58cff1fcc
sagel@bk.ru <git_rabota2017>
2017-07-24 18:04:57 +0300

    gui init(not work)

--- src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/App.java
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/App.java
@@ -1,9 +1,48 @@
 package com.taxtelecom.chelnyedu.dropwizardclient;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import java.io.IOException;
+
 /**
  * Created by user on 18.07.17.
  */
-public class App {
+public class App  extends Application{
+    private Stage primaryStage;
+    private Pane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("PhoneBook");
+
+        initRootLayout();
+    }
+    public void initRootLayout() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("guiform/PhoneBookForm.fxml"));
+            rootLayout = loader.load();
+
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
-        System.out.println("куку");
+        //System.out.println("куку");
+        launch(args);
     }
 }
---
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizardclient/guiform/PhoneBookForm.java
@@ -0,0 +1,7 @@
+package com.taxtelecom.chelnyedu.dropwizardclient.guiform;
+
+/**
+ * Created by user on 24.07.17.
+ */
+public class PhoneBookForm {
+}
